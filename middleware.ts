bbde76ca7ef15a7a61704cdd8aa328cbe758eecf
nick5616 <nickbelovoskey@gmail.com>
2026-03-30 13:40:06 -0700

    fix redirect issue?

--- middleware.ts
+++ middleware.ts
@@ -9,3 +9,9 @@ export function middleware(request: NextRequest) {
 
     return NextResponse.next();
 }
+
+export const config = {
+    matcher: [
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    ],
+};
